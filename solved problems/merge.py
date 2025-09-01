e8080b6fbee4750dad3c91277ae237f19a2e40bc
cd <premnaths2002@gmail.com>
2025-09-01 09:30:13 +0530

    program

--- solved problems/merge.py
+++ solved problems/merge.py
@@ -2,5 +2,7 @@
 dic_1 = {'a' :10 ,'b':20 ,'c':30}
 dic_2= {'d':50,'a':10,'e':90}
 merge_dic = dic_1.copy() 
-merge_dic.update(dic_2)
+for k,v in dic_2.items():
+    merge_dic[k] = merge_dic.get(k,0)+v
+
 print(f"the  result {merge_dic}")
